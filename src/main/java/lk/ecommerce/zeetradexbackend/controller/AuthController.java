2b6e10584da0581d67ad8eb0f050ca799ce481fb
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-03-14 16:19:10 +0530

    Create SignIn

--- src/main/java/lk/ecommerce/zeetradexbackend/controller/AuthController.java
+++ src/main/java/lk/ecommerce/zeetradexbackend/controller/AuthController.java
@@ -4,12 +4,16 @@ import lk.ecommerce.zeetradexbackend.config.JwtProvider;
 import lk.ecommerce.zeetradexbackend.entity.User;
 import lk.ecommerce.zeetradexbackend.response.AuthResponse;
 import lk.ecommerce.zeetradexbackend.repo.UserRepo;
+import lk.ecommerce.zeetradexbackend.service.CustomeUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CachingUserDetailsService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private CustomeUserDetailsService customeUserDetailsService;
 
     @PostMapping("/signup")
     public ResponseEntity<AuthResponse> registerUser(@RequestBody User user) {
@@ -57,5 +64,43 @@ public class AuthController {
         return new ResponseEntity<>(res, HttpStatus.CREATED);
 
         
+    }
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> login(@RequestBody User user) {
+
+        String userName = user.getEmail();
+        String password = user.getPassword();
+
+
+        Authentication auth = authenticate(userName, password);
+        
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        //after creating jwt token we need to send it to the fontend
+        String jwt= JwtProvider.generateToken(auth);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("login success");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+
+    }
+
+    //check user isExist
+    private Authentication authenticate(String userName, String password) {
+        UserDetails userDetails = customeUserDetailsService.loadUserByUsername(userName);
+
+        if (userDetails == null) {
+            throw new BadCredentialsException("invalid username or password");
+        }
+        if (!password.equals(userDetails.getPassword())) {
+            throw new BadCredentialsException("invalid password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+
     }
 }
